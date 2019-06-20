5275149723db66f9a2f76e38e63b6df9427c409e
Will Taylor <willtaylor@Hyperion.local>
2019-06-20 14:56:57 -0700

    posting and getting list works, in development: retrieval

--- controller/photo_library.py
+++ controller/photo_library.py
@@ -5,9 +5,11 @@ import uuid #read
 import os
 import io
 
-class Library(object):
+_CHUNK_SIZE_IN_BYTES = 4096 # maximum byte size of an inputted image
 
-    _CHUNK_SIZE_IN_BYTES = 4096 # maximum byte size of an inputted image
+class Library(object):
+    
+
 
     def __init__(self, save_path):
         self.save_path = save_path
@@ -19,10 +21,17 @@ class Library(object):
         self.path = os.path.join(os.path.realpath('..'), 'falcon_single_controller/savedImages')
         for root, directory, file in os.walk(self.path):
             if len(file) is not 0:
+                #index into file name
                self.image_document['images'].append('href: ' + file[0])
     
+    
     def on_get(self, req, resp):
-        #currently returns entirety of image document
+        # if image_name is not None:
+        #     self.get_image(req, resp, image_name)
+        # else:
+        # #currently returns entirety of image document
+        #     resp.body = json.dumps(self.image_document, ensure_ascii=False)
+        #     resp.status = falcon.HTTP_200
         resp.body = json.dumps(self.image_document, ensure_ascii=False)
         resp.status = falcon.HTTP_200
 
@@ -30,18 +39,29 @@ class Library(object):
         #sanitize input for only image file types? currently doesn't
         #currently requires content type is specified as image png, otherwise assumes JSON?
         extension = mimetypes.guess_extension(req.content_type)
-        image_name = '{uuid}{ext}'.format(uuid=uuid.uuid4(), ext=extension)
-        image_path = os.path.join(self.save_path, image_name)
+        if extension != '.png': #todo fix this to return properly formed http 400 response
+            error_message = 'Content-Type is not specified as proper image type of png, instead of type ' + str(extension)
+            raise falcon.HTTPBadRequest('400 Bad Request', error_message)
+        else:
+            image_name = '{uuid}{ext}'.format(uuid=uuid.uuid4(), ext=extension)
+            image_path = os.path.join(self.save_path, image_name)
 
-        with io.open(image_path, 'wb') as image_file:
-            while True:
-                chunk = req.stream.read(self._CHUNK_SIZE_IN_BYTES)
-                if not chunk:
-                    break
-                image_file.write(chunk)
-            self.image_document['images'].append('href: ' + image_name)
-            resp.status = falcon.HTTP_200
-            resp.location = '/images/' + image_name
+            with io.open(image_path, 'wb') as image_file:
+                while True:
+                    chunk = req.stream.read(_CHUNK_SIZE_IN_BYTES)
+                    if not chunk:
+                        break
+                    image_file.write(chunk)
+                self.image_document['images'].append('href: ' + image_name)
+                resp.status = falcon.HTTP_200
+                resp.location = '/images/' + image_name
 
-    #def on_delete(self, req, resp):
-        #image_to_delete = req. how to index to one particular image / parse a req file
+
+class Image(object):
+    #get to return one specific image instead of the list of them
+    def on_get(self, req, resp, image_name):
+        if Library.image_document['images'].__contains__(image_name):
+            image_path = os.path.join(Library.save_path, image_name)
+            stream = io.open(image_path, 'rb')
+            content_length = os.path.getsize(image_path)
+            return stream, content_length
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from controller.photo_library import Library
 
 #set-up logging
 
-#app.add_route()s to add functionality : get & post & delete(if time)
+#app.add_route()s to add functionality : get & post & delete(if)
 api = application = falcon.API()
 images = Library('./savedImages') # need a file-path
 api.add_route('/images', images)
